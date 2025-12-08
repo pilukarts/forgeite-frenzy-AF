9cbb8fdd08b4a62107adcf35382f8d6137b86afb
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-12-08 21:06:44 +0000

    quiero todos esos si

--- src/components/navigation/SidebarNav.tsx
+++ src/components/navigation/SidebarNav.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import Link from 'next/link';
 import { Home, ChevronsUp, Trophy, Users, ShoppingCart, MessagesSquare, ListChecks, Swords, Map, Gamepad2, FileText, UserCircle, GalleryHorizontal, LifeBuoy, Info, Replace, Music, Music2, RefreshCw, Globe, Share2, Send, Bot } from 'lucide-react';
@@ -113,3 +112,5 @@ const SidebarNav: React.FC = () => {
 };
 
 export default SidebarNav;
+
+    
